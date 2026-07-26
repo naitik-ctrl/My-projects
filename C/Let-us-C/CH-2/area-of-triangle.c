ca34e2e9e0a3dd0e95cac960840e00303e0f1be0
NAITIK BARIA <naitikbaria77@gmail.com>
2026-07-26 15:53:11 +0530

    Update area-of-triangle.c

--- C/Let-us-C/CH-2/area-of-triangle.c
+++ C/Let-us-C/CH-2/area-of-triangle.c
@@ -1,1 +1,14 @@
+/*Including the header-files*/
+#include <stdio.h>
+#include <math.h>
 
+int main()
+{
+    float a,b,c,sp,area; /*sp stands for semi-perimeter*/
+    printf("\nEnter the sides of traingle: ");
+    scanf("%f %f %f", &a, &b, &c);
+    sp = (a+b+c)/2;
+    area = sqrt(sp*(sp-a)*(sp-b)*(sp-c));
+    printf("The area of the triangle given is: %f\n", area);
+    return 0;
+}
