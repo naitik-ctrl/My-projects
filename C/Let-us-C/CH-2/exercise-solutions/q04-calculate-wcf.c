7c6cf6cf8f24970071b6c8445d788df2d90ee823
NAITIK BARIA <naitikbaria77@gmail.com>
2026-07-27 16:38:54 +0530

    Implement wind-chill factor calculation program
    Added a program to calculate the wind-chill factor based on temperature and wind velocity inputs.

--- C/Let-us-C/CH-2/exercise-solutions/q04-calculate-wcf.c
+++ C/Let-us-C/CH-2/exercise-solutions/q04-calculate-wcf.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <math.h>
 
+int main()
+{
+    /* Read data from the user*/
+    float t, v, wcf;
+    printf("\nEnter the temperature of air in your area: \n");
+    scanf("%f", &t);
+    printf("\nEnter the wind velocity in your area: \n");
+    scanf("%f", &v);
+    /* Apply the provided formula*/
+    wcf = 35.74 + (0.6215*t) + ((0.4275*t) - 35.75) * pow(v, 0.16);
+    /* Display the result*/
+    printf("The Wind-chill factor in your area is: %f", wcf);
+    return 0;
+}
