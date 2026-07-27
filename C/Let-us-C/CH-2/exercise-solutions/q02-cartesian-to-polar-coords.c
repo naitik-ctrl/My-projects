da84fa0790363aa2a5cbaef1bdb22b501d41022c
NAITIK BARIA <naitikbaria77@gmail.com>
2026-07-27 15:53:10 +0530

    Implement Cartesian to Polar Coordinates conversion

--- C/Let-us-C/CH-2/exercise-solutions/q02-cartesian-to-polar-coords.c
+++ C/Let-us-C/CH-2/exercise-solutions/q02-cartesian-to-polar-coords.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <math.h>
 
+int main()
+{
+    /*Declare the variables*/
+    float x, y, r, phi;
+    /*Input x, y from the user*/
+    /* Note: only 1st quadrant co-rdinates are accepted*/
+    printf("\nEnter the cartesian co-ordinates (x,y): \n");
+    scanf("%f %f", &x, &y);
+    /* Using the formula for finding polar co-ordinates*/
+    r = sqrt((x*x) + (y*y));
+    phi= atan2(y, x);
+    /* Display the results*/
+    printf("The polar co-ordinates are: %f, %f", r, phi);
+    return 0;
+}
