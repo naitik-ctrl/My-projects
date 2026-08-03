0f86e2fe7590199175ea7c55274a0ccd30d4a4dd
NAITIK BARIA <naitikbaria77@gmail.com>
2026-08-03 16:35:27 +0530

    Update q01-armstrong-number.c
    Removed the main function that checks for Armstrong numbers.

--- C/Let-us-C/CH-5/q01-armstrong-number.c
+++ C/Let-us-C/CH-5/q01-armstrong-number.c
@@ -30,35 +30,3 @@ int main()
     }
     return 0;
 }
-#include <stdio.h>
-
-int main()
-{
-    int i = 1;
-    int rem;
-    
-    while (i <= 500)
-    {
-        int temp = i;
-        int sum = 0;
-        
-        while (temp > 0)
-        {
-            rem = temp % 10;
-            // For numbers 1 to 9, power is 1. For 100 to 500, power is 3.
-            if (i < 10) {
-                sum = sum + rem; 
-            } else {
-                sum = sum + (rem * rem * rem);
-            }
-            temp = temp / 10;
-        }
-        
-        if (sum == i)
-        {
-            printf("%d is an armstrong number\n", i);
-        }
-        i = i + 1;
-    }
-    return 0;
-}
